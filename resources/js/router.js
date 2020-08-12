c41d97e214c806ff043addb65e85cf9c88d8ec61
Gustavo Hilario <gustavodover25@gmail.com>
2020-08-12 11:47:56 -0500

    16 newpost and newfeed components

--- resources/js/router.js
+++ resources/js/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Start from './views/Start';
+import Start from "./views/Start";
+import NewsFeed from "./views/NewsFeed";
 
 //Tell vue that we are gonna use VueRouter
 Vue.use(VueRouter);
@@ -11,7 +12,7 @@ export default new VueRouter({
 
     routes: [
         {
-            path: '/', name: 'home', component: Start
+            path: '/', name: 'home', component: NewsFeed
         }
     ]
 });
