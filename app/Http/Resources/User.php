ef48f685a959bb62ccbb2ec3520215853085fcd4
Gustavo Hilario <gustavodover25@gmail.com>
2020-08-16 11:53:06 -0500

    34. Retrieving Friendships

--- app/Http/Resources/User.php
+++ app/Http/Resources/User.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Resources;
 
+use App\Friend as Friend;
+use App\Http\Resources\Friend as FriendResource;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class User extends JsonResource
@@ -20,7 +22,8 @@ class User extends JsonResource
                 'user_id' => $this->id,
                 'attributes' => [
                     'name' => $this->name,
-                ]
+                    'friendship' => new FriendResource(Friend::friendship($this->id)),
+                ],
             ],
             'links' => [
                 'self' => url('/users/'.$this->id)
