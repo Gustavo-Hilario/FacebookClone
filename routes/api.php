be96f39960d089f158a06597bac4860bc2602cd9
Gustavo Hilario <gustavodover25@gmail.com>
2020-08-15 22:11:34 -0500

    29. Friend Request Test

--- routes/api.php
+++ routes/api.php
@@ -23,6 +23,7 @@ Route::middleware('auth:api')->group(function (){
         '/posts' => 'PostController',
         '/users' => 'UserController',
         '/users/{user}/posts' => 'UserPostController',
+        '/friend-request' => 'FriendRequestController',
     ]);
 
 });
