b1deb4d99240e58082bed5584daa8c83beb43155
Gustavo Hilario <gustavodover25@gmail.com>
2020-08-15 22:28:56 -0500

    30. Handling Invalid Users

--- tests/Feature/FriendsTest.php
+++ tests/Feature/FriendsTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Friend;
 use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -38,6 +39,27 @@ class FriendsTest extends TestCase
             ],
             'links' => [
                 'self' => url('/users/' . $anotherUser->id),
+            ]
+        ]);
+    }
+
+    /** @test */
+    public function only_valid_users_can_be_requested()
+    {
+        $this->actingAs($user = factory(User::class)->create(), 'api');
+
+        $response = $this->post('/api/friend-request', [
+            'friend_id' => 123,
+        ])->assertStatus(404);
+
+        $this->assertNull(Friend::first());
+
+        /*Error Response*/
+        $response->assertJson([
+            'error' => [
+                'code' => 404,
+                'title' => 'User Not Found',
+                'detail' => 'Unable to locate the user with the given information.'
             ]
         ]);
 
