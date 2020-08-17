0209e00295847a2bcfd022235d8a730ba99273a9
Gustavo Hilario <gustavodover25@gmail.com>
2020-08-17 17:52:02 -0500

    50. Displaying Comments on a Post

--- resources/js/components/Post.vue
+++ resources/js/components/Post.vue
@@ -31,7 +31,7 @@
             </div>
             <div class="flex items-center">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="fill-current w-5 h-5"><path d="M20.3 2H3.7C2 2 .6 3.4.6 5.2v10.1c0 1.7 1.4 3.1 3.1 3.1V23l6.6-4.6h9.9c1.7 0 3.1-1.4 3.1-3.1V5.2c.1-1.8-1.3-3.2-3-3.2zm1.8 13.3c0 1-.8 1.8-1.8 1.8H9.9L5 20.4V17H3.7c-1 0-1.8-.8-1.8-1.8v-10c0-1 .8-1.8 1.8-1.8h16.5c1 0 1.8.8 1.8 1.8v10.1zM6.7 6.7h10.6V8H6.7V6.7zm0 2.9h10.6v1.3H6.7V9.6zm0 2.8h10.6v1.3H6.7v-1.3z"/></svg>
-                <p class="ml-1">123 Comments</p>
+                <p class="ml-1">{{ post.data.attributes.comments.comment_count }} comments</p>
             </div>
         </div>
 
@@ -44,12 +44,43 @@
                 <p class="ml-2">Like</p>
             </button>
 
-            <button class="flex w-full justify-center items-center py-2 rounded-lg text-sm text-gray-700 hover:bg-gray-200">
+            <button class="flex w-full justify-center items-center py-2 rounded-lg text-sm text-gray-700 focus:outline-none"
+                    @click="comments = ! comments">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="fill-current w-5 h-5"><path d="M20.3 2H3.7C2 2 .6 3.4.6 5.2v10.1c0 1.7 1.4 3.1 3.1 3.1V23l6.6-4.6h9.9c1.7 0 3.1-1.4 3.1-3.1V5.2c.1-1.8-1.3-3.2-3-3.2zm1.8 13.3c0 1-.8 1.8-1.8 1.8H9.9L5 20.4V17H3.7c-1 0-1.8-.8-1.8-1.8v-10c0-1 .8-1.8 1.8-1.8h16.5c1 0 1.8.8 1.8 1.8v10.1zM6.7 6.7h10.6V8H6.7V6.7zm0 2.9h10.6v1.3H6.7V9.6zm0 2.8h10.6v1.3H6.7v-1.3z"/></svg>
                 <p class="ml-2">Comments</p>
             </button>
         </div>
 
+        <!--Toogle to see comments-->
+        <div v-if="comments" class="border-t border-gray-400 p-4 pt-2">
+            <div class="flex">
+                <input v-model="commentBody" type="text" name="comments" class="w-full pl-4 h-8 bg-gray-400 rounded-lg focus:outline-none">
+                <button v-if="commentBody" class="bg-gray-200 ml-2 px-2 py-1 rounded-lg focus:outline-none">
+                    Post
+                </button>
+            </div>
+
+            <!-- Displaying comments-->
+            <div class="flex my-4 items-center" v-for="comment in post.data.attributes.comments.data">
+                <div class="w-8">
+                    <img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Profile User" class="w-8 h-8 object-cover rounded-full">
+                </div>
+
+                <div class="ml-4 flex-1">
+                    <div class="bg-gray-200 rounded-lg p-2 text-sm">
+                        <a :href="'/users/' + comment.data.attributes.commented_by.data.user_id" class="font-bold text-blue-700">
+                            {{ comment.data.attributes.commented_by.data.attributes.name }}
+                        </a>
+                        <p class="inline">
+                            {{ comment.data.attributes.body }}
+                        </p>
+                    </div>
+                    <div>
+                        <p class="text-xs pl-2">{{ comment.data.attributes.commented_at }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -60,6 +91,13 @@
         props: [
             'post',
         ],
+
+        data: () => {
+            return {
+                comments:false,
+                commentBody: '',
+            }
+        }
 
     }
 </script>
